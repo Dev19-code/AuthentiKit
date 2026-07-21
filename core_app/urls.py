de9735cd40c3cbb0e916b1c2562bb8d252f72a6a
mohamedosman0111700463-ui <mohamedosman0111700463@gmail.com>
2026-07-21 21:21:57 +0200

    done

--- core_app/urls.py
+++ core_app/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
+from django.http import HttpResponse
 from . import views
+
+def signin_placeholder(request):
+    return HttpResponse("صفحة تسجيل الدخول - لسه قيد الإنشاء (accounts_app)")
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('quick-verify/', views.quick_verify_ajax, name='quick_verify'),
+    path('signin/', signin_placeholder, name='signin'),  # 🔧 مؤقت لحد ما يخلص زميلك
 ]
